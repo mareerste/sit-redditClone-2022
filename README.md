2d2542612fa22e3a6e108456acd3cdf7c8a19500
Marko Eršte <erste.sr47.2020@uns.ac.rs>
2022-04-03 18:39:36 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sit-redditClone-2022
---
+++ src/main/java/rs/ac/uns/ftn/informatika/redditClone/RedditCloneApplication.java
@@ -0,0 +1,15 @@
+package rs.ac.uns.ftn.informatika.redditClone;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class RedditCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditCloneApplication.class,args);
+    }
+
+}
