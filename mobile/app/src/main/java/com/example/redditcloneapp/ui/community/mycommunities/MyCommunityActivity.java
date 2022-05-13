9a47f2662aa9fc63198210922b8041de0863f590
Marko Eršte <erste.sr47.2020@uns.ac.rs>
2022-05-14 00:22:39 +0200

    feat: Suspend community dialog

--- mobile/app/src/main/java/com/example/redditcloneapp/ui/community/mycommunities/MyCommunityActivity.java
+++ mobile/app/src/main/java/com/example/redditcloneapp/ui/community/mycommunities/MyCommunityActivity.java
@@ -2,9 +2,12 @@ package com.example.redditcloneapp.ui.community.mycommunities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.redditcloneapp.R;
 import com.example.redditcloneapp.model.Community;
@@ -63,6 +66,42 @@ public class MyCommunityActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 FragmentTransition.to(CommunityFlairsEditFragment.newInstance(), MyCommunityActivity.this,false,R.id.my_community_fragment);
+            }
+        });
+
+        Button btnSuspend = findViewById(R.id.my_community_suspend_comm_btn);
+        btnSuspend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = new Dialog(MyCommunityActivity.this);
+                dialog.setContentView(R.layout.dialog_ban_or_view_user);
+                TextView text = dialog.findViewById(R.id.dialog_ban_or_view_text);
+                text.setText(community.getName());
+                Button btnLeft = dialog.findViewById(R.id.dialog_ban_or_view_btn_left);
+                btnLeft.setText("Suspend");
+                Button btnRight = dialog.findViewById(R.id.dialog_ban_or_view_btn_right);
+                btnRight.setText(R.string.cancel);
+                EditText susReason = dialog.findViewById(R.id.dialog_ban_or_view_edit_text);
+                susReason.setVisibility(View.VISIBLE);
+                susReason.setHint(R.string.sus_reason);
+                dialog.show();
+                btnRight.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+                btnLeft.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(!susReason.getText().toString().equals("")){
+                            community.setSuspended(true);
+                            community.setSuspendedReason(susReason.getText().toString());
+                            dialog.dismiss();
+                            FragmentTransition.to(CommunityBasicInfoFragment.newInstance(), MyCommunityActivity.this,false,R.id.my_community_fragment);
+                        }
+                    }
+                });
             }
         });
     }
